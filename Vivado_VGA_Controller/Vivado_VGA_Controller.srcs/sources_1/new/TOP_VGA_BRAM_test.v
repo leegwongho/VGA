45c089875c5a8a53fc8e9795df8dc050268c9598
ysk <lotenimas@gmail.com>
2024-08-20 15:48:47 +0900

    Update TOP_VGA_BRAM_test.v
    changed address naming

--- Vivado_VGA_Controller/Vivado_VGA_Controller.srcs/sources_1/new/TOP_VGA_BRAM_test.v
+++ Vivado_VGA_Controller/Vivado_VGA_Controller.srcs/sources_1/new/TOP_VGA_BRAM_test.v
@@ -9,7 +9,7 @@ module TOP_VGA_BRAM_test (
 
     wire [3:0] inter_vgaRed, inter_vgaGreen, inter_vgaBlue;
     wire [3:0] gen_vgaRed, gen_vgaGreen, gen_vgaBlue;
-    wire [9:0] addr_x, addr_y;
+    wire [9:0] gen_addr_x, gen_addr_y;
     wire write_enable;
     wire display_on;
     wire [9:0] cur_pixel_coord_x, cur_pixel_coord_y;
@@ -63,8 +63,8 @@ module TOP_VGA_BRAM_test (
        .porta_vgaGreen (gen_vgaGreen),
        .porta_vgaBlue (gen_vgaBlue),
        
-       .in_addr_x (addr_x),
-       .in_addr_y (addr_y),
+       .in_addr_x (gen_addr_x),
+       .in_addr_y (gen_addr_y),
        
        .out_addr_x (cur_pixel_coord_x),
        .out_addr_y (cur_pixel_coord_y),
@@ -82,8 +82,8 @@ module TOP_VGA_BRAM_test (
         .in_vsync (Vsync),
         .in_display_on (display_on),
         
-        .out_addr_x (cur_pixel_coord_x),
-        .out_addr_y (cur_pixel_coord_y),
+        .out_addr_x (gen_addr_x),
+        .out_addr_y (gen_addr_y),
         .write_enable (write_enable),
         
         .vgaRed (gen_vgaRed),
