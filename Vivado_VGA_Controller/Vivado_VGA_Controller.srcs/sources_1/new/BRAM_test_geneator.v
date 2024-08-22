cf6cd40b1b37fcfe446d3b4ea499145cafb8ac0b
DolySager <lotenimas@gmail.com>
2024-08-22 17:53:56 +0900

    Revert "revert_commit"
    This reverts commit eb020d05997ee4d2098aca90a321208ea9a595ef.

--- Vivado_VGA_Controller/Vivado_VGA_Controller.srcs/sources_1/new/BRAM_test_geneator.v
+++ Vivado_VGA_Controller/Vivado_VGA_Controller.srcs/sources_1/new/BRAM_test_geneator.v
@@ -6,8 +6,8 @@ module BRAM_test_generator (
     input pixel_clock_pulse,
     input in_hsync, in_vsync,
     input in_display_on,
-    output reg [8:0] out_addr_x,
-    output reg [7:0] out_addr_y,
+    output reg [9:0] out_addr_x,
+    output reg [9:0] out_addr_y,
     output reg write_enable,
     output [3:0] vgaRed, vgaGreen, vgaBlue
 );
@@ -17,6 +17,7 @@ module BRAM_test_generator (
     edge_detector_n vsync_edge_detector (.clk(clk), .reset_p(reset_p), .cp(in_vsync), .n_edge(vsync_nedge));
 
     reg [3:0] counter_red, counter_green, counter_blue;
+    integer counter;
     //integer pixel_counter;
     
     // address manipulator
@@ -24,45 +25,47 @@ module BRAM_test_generator (
         if (reset_p) begin
             out_addr_x = 0;
             out_addr_y = 0;
+            
         end
         else begin
-            if (pixel_clock_pulse && in_display_on) begin
+            if (write_enable) begin
                 out_addr_x = out_addr_x + 1;
-                if (out_addr_x >= 9'd320) out_addr_x = 0;
+                if (out_addr_x >= 320) begin
+                    out_addr_x = 0;
+                    out_addr_y = out_addr_y + 1;
+                end
             end
-            else if (hsync_nedge) begin
-                out_addr_x = 0;
-                out_addr_y = out_addr_y + 1;
-                if (out_addr_y >= 8'd240) out_addr_y = 0;   // If plain 240 was put instead of 8'd240, it is considered signed, thus becomes -16.
-            end
-            else if (vsync_nedge) begin
+            else begin
                 out_addr_x = 0;
                 out_addr_y = 0;
             end
         end
     end
-    
-    // write enable (initially 1, changed to 0 after writing BRAM)
+
     always @(negedge clk or posedge reset_p) begin
         if (reset_p) begin
             write_enable = 1;
         end
-        else begin
-            if (vsync_nedge) begin
+        else if (write_enable) begin
+            if (out_addr_y >= 240) begin
                 write_enable = 0;
             end
         end
+        else if (vsync_nedge) begin
+            write_enable = 1;
+        end
     end
-        
+
     always @(negedge clk or posedge reset_p) begin
         if (reset_p) begin
-            //pixel_counter = 0;
             counter_red = 0;
             counter_green = 0;
             counter_blue = 0;
+            counter = 0;
         end
-        else if (pixel_clock_pulse && in_display_on) begin
+        else begin
             if (write_enable) begin
+                /*
                 if(out_addr_x <= 160)begin
                     counter_red = 4'b1111;
                     counter_green = 4'b0000;
@@ -73,10 +76,13 @@ module BRAM_test_generator (
                     counter_green = 4'b0000;
                     counter_blue = 4'b0000;
                 end
+                */
+                counter_red = counter >> 1;
+                counter_green = counter << 1;
+                counter_blue = counter >> 2;
+                counter = counter + 1;
             end
         end
-        
-        //pixel_counter = pixel_counter + 1;
     end
     
     assign vgaRed = counter_red;
